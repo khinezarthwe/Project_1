a449d8c55c4ea23ed502012c7ce7d8fc3311f271
khinezarthwe <koalakhine@gmail.com>
2015-12-19 13:06:32 +0900

    correcting error

--- LDA_result_from_songdata.rb
+++ LDA_result_from_songdata.rb
@@ -6,10 +6,13 @@ require 'minitest/autorun'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-class songlda< Minitest::Test
+
+class TestMeme < Minitest::Test
+
 	@corpus = Lda::Corpus.new
-	file = File.open("data/song_lyric_1", "r")
-	@songdata = Lda::TextDocument.new (@corpus,file.read)
+	file = File.open("data/song_lyric_1.txt", "r")
+	@songdata = Lda::TextDocument.new(@corpus,file.read)
+	@corpus.add_document(@songdata)
 	@lda = Lda::Lda.new(@corpus)
 	@lda.num_topics = 5
 	@lda.em('random')
